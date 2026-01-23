ccd8408d6e7f53f42522a8ba6357db76925bbf71
Mohammad Yusuf Ali <59096473+Yukii-Codes@users.noreply.github.com>
2026-01-23 15:00:56 +0530

    Implement basic Java initialization and output
    Added basic initialization and printing of variables.

--- beginner/java-basics/JavaFundamentals.java
+++ beginner/java-basics/JavaFundamentals.java
@@ -1,1 +1,13 @@
-
+//Basic initialization and printing 
+public class JavaFundamentals {
+    public static void main(String[] args) {
+       int number1 = 140;
+       double number2 = 5.0;
+       char character = 'F';
+       String introduction = "Hello, I am Yusuf";
+       System.out.println(" Integer Number : " + number1);
+       System.out.println(" Decimal Number : " + number2);
+       System.out.println(" Single Character : " + character);
+       System.out.println(" String Statement : " + introduction);
+    }
+}
