72a4c9c812e76f1b11ec18f127ea61702392dc54
Mohammad Yusuf Ali <59096473+Yukii-Codes@users.noreply.github.com>
2026-01-23 15:18:54 +0530

    Create SimpleCalculations.java

---
+++ beginner/java-basics/SimpleCalculations.java
@@ -0,0 +1,16 @@
+public class SimpleCalculation {
+    public static void main(String[]args){
+        int number1 = 25;
+        int number2 = 2;
+        int addition = number1 + number2;
+        int substraction = number1 - number2;
+        int multiplication = number1 * number2;
+        double division = (double) number1 / number2;
+        double remainder = number1 % number2;
+        System.out.println(" Addition : " + addition);
+        System.out.println(" Substraction : " + substraction);
+        System.out.println(" Multipication : " + multiplication);
+        System.out.println(" Division : " + division);
+        System.out.println(" Remainder : " + remainder);
+    }
+}
